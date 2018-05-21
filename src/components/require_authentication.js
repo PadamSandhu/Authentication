f52fb4bea1ec3aed2929c1f17df1e2dc75eb8ef9
PadamSandhu <sandhp7@gmail.com>
2018-05-21 11:16:54 -0400

    Updated Programmatic Routing

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux";
+import PropTypes from 'prop-types';
 
 export default function (ComposedComponent) {
     class Authentication extends Component {
+        // static contextTypes = {
+        //     router: PropTypes.object.isRequired
+        //   }
+          componentWillMount() {
+            //   this.context.router.history.push('/')
+            if(!this.props.authenticated){
+                this.props.history.push('/');
+            }
+          }
+        
         render() {
+            console.log(this.context);
             return <ComposedComponent {...this.props}/>
         }
     }
+
 
     function mapStateToProps(state) {
         return { 
